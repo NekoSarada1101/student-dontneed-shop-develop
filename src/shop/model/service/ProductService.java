7d6d702bd2cdb835c4c66935ba823acae2a155e2
Sarada1101 <ryoyukikazu11@gmail.com>
2020-06-18 11:57:53 +0900

    update:fetchGenreInfoメソッド #5

--- src/shop/model/service/ProductService.java
+++ src/shop/model/service/ProductService.java
@@ -1,4 +1,15 @@
 package shop.model.service;
 
+import shop.model.dao.ProductDao;
+
+import java.util.List;
+import java.util.Map;
+
 public class ProductService extends CommonService {
+
+    private ProductDao productDao = new ProductDao();
+
+    public List<Map<String, Object>> fetchGenreInfo() {
+        return productDao.fetchGenreInfo();
+    }
 }
