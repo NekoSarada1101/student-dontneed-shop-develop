72a0391f20b5e8260b3a073b791b92f6d08c6547
Sarada1101 <ryoyukikazu11@gmail.com>
2020-06-22 14:37:51 +0900

    create:AdminBeans #10

---
+++ src/shop/model/bean/AdminBeans.java
@@ -0,0 +1,51 @@
+package shop.model.bean;
+
+import java.lang.annotation.Repeatable;
+
+public class AdminBeans {
+    private String adminMail = null;
+    private String adminName = null;
+    private String adminPassword = null;
+    private int postalCode = 0;
+    private String address = null;
+
+    public String getAdminMail() {
+        return adminMail;
+    }
+
+    public void setAdminMail(String adminMail) {
+        this.adminMail = adminMail;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
