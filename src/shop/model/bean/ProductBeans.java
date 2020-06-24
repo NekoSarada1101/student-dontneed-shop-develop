75c968385de7f18a4c887f23c8fa5177fdd38b51
Sarada1101 <ryoyukikazu11@gmail.com>
2020-06-24 15:09:09 +0900

    update:画像をバイト配列で扱う #15

--- src/shop/model/bean/ProductBeans.java
+++ src/shop/model/bean/ProductBeans.java
@@ -1,12 +1,11 @@
 package shop.model.bean;
 
-import java.awt.image.BufferedImage;
 
 public class ProductBeans {
     private int productId = 0;
     private String productName = null;
     private int price = 0;
-    private BufferedImage image = null;
+    private byte[] image = null;
     private String productExplanation = null;
     private boolean isSold = false;
     private int genreCode = 0;
@@ -36,12 +35,12 @@ public class ProductBeans {
         this.price = price;
     }
 
-    public void getImage() {
-        this.image = image;
+    public byte[] getImage() {
+        return image;
     }
 
-    public BufferedImage setImage(BufferedImage image) {
-        return image;
+    public void setImage(byte[] image) {
+        this.image = image;
     }
 
     public String getProductExplanation() {
