c79d058191779d8120b7adb284d71903cfbf7ce5
Tsuuugachi <1801169@s.asojuku.ac.jp>
2020-06-25 15:34:42 +0900

    create:管理者ログイン(UserDao,UserService)#4

--- src/shop/model/dao/UserDao.java
+++ src/shop/model/dao/UserDao.java
@@ -1,4 +1,42 @@
 package shop.model.dao;
 
+import java.sql.SQLException;
+
+import shop.model.bean.AdminBeans;
+
 public class UserDao extends DaoBase {
+	public AdminBeans fetchAdminLogin(String admin_mail,String password){
+		//戻り値を初期化
+		AdminBeans adminBeans = null;
+		try{
+			//DBのオープン
+			super.connect();
+			//SQLを作成
+			String sql = "SELECT * FROM student_dontneed_shop WHERE admin_mail=? AND admin_password=?";
+			stmt = this.con.prepareStatement(sql);
+			stmt.setString(1,admin_mail);
+			stmt.setString(2, password);
+
+			//SQLの発行
+			rs = this.stmt.executeQuery();
+			rs.next();
+
+			//beanへ格納(検索失敗ならrs.nextでcatchへ飛ぶ)
+			adminBeans = new AdminBeans();
+			adminBeans.setAdminMail(this.rs.getString("adminMail"));
+			adminBeans.setAdminPassword(this.rs.getString("adminPassword"));
+
+		}catch (SQLException e) {
+			e.printStackTrace();
+			adminBeans = null;
+		}finally {
+
+			try {
+				super.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return adminBeans;
+	}
 }
--- src/shop/model/service/UserService.java
+++ src/shop/model/service/UserService.java
@@ -1,5 +1,21 @@
 package shop.model.service;
 
+import shop.model.bean.AdminBeans;
+import shop.model.dao.UserDao;
+
 public class UserService extends CommonService {
+
+	public AdminBeans fetchAdminLogin(String adminMail,String adminPassword) {
+
+		//戻り値を初期化
+		AdminBeans adminBeans = null;
+		shop.model.dao.UserDao userDao = new UserDao();
+
+		adminBeans = new AdminBeans();
+
+		adminBeans = userDao.fetchAdminLogin(adminMail,adminPassword);
+
+		return adminBeans;
+	}
 
 }
