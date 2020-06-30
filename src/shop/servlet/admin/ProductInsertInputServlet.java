e35b038f0f3e8f5ada4a4eb604e0fb1f945be904
Sarada1101 <ryoyukikazu11@gmail.com>
2020-06-30 13:55:03 +0900

    fix:トップ画面から遷移したときにプレビューを表示しないように修正

--- src/shop/servlet/admin/ProductInsertInputServlet.java
+++ src/shop/servlet/admin/ProductInsertInputServlet.java
@@ -7,15 +7,24 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/productInsertInput")
 public class ProductInsertInputServlet extends HttpServlet {
 
-    ProductService productService = new ProductService();
+    private ProductService productService = new ProductService();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.removeAttribute("productBeans");
+
+        request.getRequestDispatcher("WEB-INF/jsp/admin/product_insert_input.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("WEB-INF/jsp/admin/product_insert_input.jsp").forward(request, response);
     }
 }
