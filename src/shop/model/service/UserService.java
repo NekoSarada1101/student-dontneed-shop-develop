b70ecbfb084039256dd47115950114386ea26399
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-07 11:14:12 +0900

    fix:まーじ前の状態に戻す

--- src/shop/model/service/UserService.java
+++ src/shop/model/service/UserService.java
@@ -1,40 +1,12 @@
+
 package shop.model.service;
 
-<<<<<<< HEAD
-=======
 import shop.model.bean.AdminBeans;
 import shop.model.bean.MemberBeans;
 import shop.model.dao.UserDao;
 
 public class UserService extends CommonService {
 
-<<<<<<< HEAD
-
-	public boolean checkMemberMailExists(String memberMail ) {
-
-		UserDao userDao = new UserDao();
-
-		boolean exists = userDao.checkMemberMailExists(memberMail);
-
-		return exists;
-
-	}
-
-
-
-
-
-	public boolean insertMember(MemberBeans membeerBeans){
-
-		UserDao userDao = new UserDao();
-
-		boolean couldInsert = userDao.insertMember(membeerBeans);
-
-		return couldInsert;
-	}
-
-
-=======
     private UserDao userDao = new UserDao();
 
     public AdminBeans fetchAdminLogin(String adminMail, String adminPassword) {
