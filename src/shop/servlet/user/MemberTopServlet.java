af817d070c8d882c5826d0333cec977610fe9efe
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-07 14:25:08 +0900

    fix

--- src/shop/servlet/user/MemberTopServlet.java
+++ src/shop/servlet/user/MemberTopServlet.java
@@ -1,7 +1,9 @@
+
 package shop.servlet.user;
 
-import shop.model.bean.MemberBeans;
-import shop.model.service.ProductService;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,9 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+
+import shop.model.bean.MemberBeans;
+import shop.model.service.ProductService;
 
 @WebServlet("/memberTop")
 public class MemberTopServlet extends HttpServlet {
--- src/shop/servlet/user/ProductSearchAndDisplayServlet.java
+++ src/shop/servlet/user/ProductSearchAndDisplayServlet.java
@@ -1,7 +1,8 @@
+
 package shop.servlet.user;
 
-import java.io.IOException;
-import java.util.List;
+import shop.model.bean.ProductBeans;
+import shop.model.service.ProductService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,36 +10,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
 
-import shop.model.bean.ProductBeans;
-import shop.model.service.ProductService;
+@WebServlet("/productSearchAndDisplay")
+public class ProductSearchAndDisplayServlet extends HttpServlet {
 
-@WebServlet("/ProductSearchAndDisplayServlet")
-public class ProductSearchAndDisplayServlet extends HttpServlet{
+    ProductService productService = new ProductService();
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
 
+        int genreCode = Integer.parseInt(request.getParameter("genreCode"));
+        String sortColumn = request.getParameter("sortColumn");
+        String sortOrder = request.getParameter("sortOrder");
+        String searchWord = request.getParameter("searchWord");
 
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		ProductService productService = new ProductService();
-		HttpSession session = request.getSession();
-
-		//String genre = request.getParameter("searchGenre");
-		//int genreCode = Integer.parseInt(genre);
-		//String sortColumn  = request.getParameter("sortColumn");
-		//String sortOrder   = request.getParameter("sortOrder");
-		//String searchWord  = request.getParameter("searchWord");
-
-		int    genreCode = 0000;
-		String sortColumn  = "price";//pirice,product_id,product_name
-		String sortOrder   = "ASC";//DESC
-		String searchWord  = "制服";
-
-		List<ProductBeans>productList = productService.fetchSearchProductList(genreCode, sortColumn, sortOrder, searchWord);
-		session.setAttribute("productList", productList);
-		request.getRequestDispatcher("WEB-INF/jsp/search_product_list.jsp").forward(request,response);
-	}
+        List<ProductBeans> productList = productService.fetchSearchProductList(genreCode, sortColumn, sortOrder, searchWord);
+        session.setAttribute("productList", productList);
+        request.getRequestDispatcher("WEB-INF/jsp/user/search_product_list.jsp").forward(request, response);
+    }
 }
-
-
