87b4c3d3be41dec234fa7b667ee1484d78908937
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-10 11:50:51 +0900

    fix:フォーマット

--- src/shop/servlet/admin/AdminLoginServlet.java
+++ src/shop/servlet/admin/AdminLoginServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-
 @WebServlet("/adminLogin")
 public class AdminLoginServlet extends HttpServlet {
 
