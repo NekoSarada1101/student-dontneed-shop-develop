dcdd9264fb584a1177d476efcd92ac37ec02bdd1
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-13 11:56:36 +0900

    udpate:memberUpdateCheckServlet

--- src/shop/servlet/user/MemberUpdateCheckServlet.java
+++ src/shop/servlet/user/MemberUpdateCheckServlet.java
@@ -1,70 +1,46 @@
 package shop.servlet.user;
 
-import java.io.IOException;
+import shop.model.bean.MemberBeans;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import shop.model.bean.MemberBeans;
+import java.io.IOException;
 
 @WebServlet("/memberUpdateCheck")
 public class MemberUpdateCheckServlet extends HttpServlet {
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
 
-		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String memberMail     = request.getParameter("memberMail");
+        String memberPassword = request.getParameter("memberPassword");
+        String memberName     = request.getParameter("memberName");
+        String postalCode     = request.getParameter("postalCode");
+        String address        = request.getParameter("address");
+        String tell           = request.getParameter("tell");
+        String creditCard     = request.getParameter("creditCard");
+        String expirationDate = request.getParameter("expirationDate");
+        String holder         = request.getParameter("holder");
+        String securityCode   = request.getParameter("securityCode");
 
-			HttpSession session = request.getSession();
+        MemberBeans memberBeans = (MemberBeans) session.getAttribute("memberBeans");
+        memberBeans.setMemberMail(memberMail);
+        memberBeans.setMemberPassword(memberPassword);
+        memberBeans.setMemberName(memberName);
+        memberBeans.setPostalCode(postalCode);
+        memberBeans.setAddress(address);
+        memberBeans.setTell(tell);
+        memberBeans.setCreditCard(creditCard);
+        memberBeans.setExpirationDate(expirationDate);
+        memberBeans.setHolder(holder);
+        memberBeans.setSecurityCode(securityCode);
 
-			MemberBeans memberBeans = new MemberBeans();
+        session.setAttribute("memberBeans", memberBeans);
 
-
-			//会員メールアドレス
-			String member_mail = request.getParameter("memberMail");
-			//会員氏名
-			String member_name = request.getParameter("memberName");
-			//会員パスワード
-			String member_password = request.getParameter("memberPassword");
-			//住所
-			String address = request.getParameter("address");
-			//郵便番号
-			int postal_code = Integer.parseInt(request.getParameter("postalCode"));
-			//電話番号
-			int tell = Integer.parseInt(request.getParameter("tell"));
-			//クレジットコード
-			int creditCard = Integer.parseInt(request.getParameter("creditCard"));
-			//有効期限
-			String deadline = request.getParameter("expirationDate");
-			//セキュリティコード
-			int security_code = Integer.parseInt(request.getParameter("securityCard"));
-			//名義者
-			String security_name = request.getParameter("holder");
-
-
-
-			memberBeans.setMemberMail(member_mail);
-			memberBeans.setMemberName(member_name);
-			memberBeans.setMemberPassword(member_password);
-			memberBeans.setAddress(address);
-			memberBeans.setPostalCode(postal_code);
-			memberBeans.setTell(tell);
-			memberBeans.setExpirationDate(deadline);
-			memberBeans.setCreditCard(creditCard);
-			memberBeans.setSecurityCard(security_code);
-			memberBeans.setHolder(security_name);
-
-
-
-
-
-			session.setAttribute("memberBeans",memberBeans);
-
-			// member_insert_check.jsp にページ遷移
-					RequestDispatcher dispatch = request.getRequestDispatcher("WEB-INF/jsp/user/member_insert_check.jsp");
-					dispatch.forward(request, response);
-		}
+        request.getRequestDispatcher("WEB-INF/jsp/user/member_update_check.jsp").forward(request, response);
+    }
 }
