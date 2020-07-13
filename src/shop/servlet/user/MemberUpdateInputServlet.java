c50533d8bb206b5360552ffcfe0db871b45131db
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-13 11:56:26 +0900

    update:memberUpdateINputServlet

--- src/shop/servlet/user/MemberUpdateInputServlet.java
+++ src/shop/servlet/user/MemberUpdateInputServlet.java
@@ -1,34 +1,36 @@
 package shop.servlet.user;
 
-import java.io.IOException;
+import shop.model.bean.MemberBeans;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
-import shop.model.bean.MemberBeans;
-
-@WebServlet("/membeUpdateInput")
+@WebServlet("/memberUpdateInput")
 public class MemberUpdateInputServlet extends HttpServlet {
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session         = request.getSession();
+        MemberBeans memberBeans     = new MemberBeans();
+        MemberBeans memberLoginInfo = (MemberBeans) session.getAttribute("memberLoginInfo");
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        memberBeans.setMemberMail(memberLoginInfo.getMemberMail());
+        memberBeans.setMemberPassword(memberLoginInfo.getMemberPassword());
+        memberBeans.setMemberName(memberLoginInfo.getMemberName());
+        memberBeans.setPostalCode(memberLoginInfo.getPostalCode());
+        memberBeans.setAddress(memberLoginInfo.getAddress());
+        memberBeans.setTell(memberLoginInfo.getTell());
+        memberBeans.setCreditCard(memberLoginInfo.getCreditCard());
+        memberBeans.setExpirationDate(memberLoginInfo.getExpirationDate());
+        memberBeans.setHolder(memberLoginInfo.getHolder());
+        memberBeans.setSecurityCode(memberLoginInfo.getSecurityCode());
 
-		HttpSession session = request.getSession();
-		MemberBeans memberBeans = new MemberBeans();
+        session.setAttribute("memberBeans", memberBeans);
 
-		memberBeans = (MemberBeans) session.getAttribute("memberLoginInfo");
-
-		session.setAttribute("memberBeans",memberBeans);
-
-
-		// member_update_input.jsp にページ遷移
-				RequestDispatcher dispatch = request.getRequestDispatcher("WEB-INF/jsp/user/member_update_input.jsp");
-				dispatch.forward(request, response);
-	}
-
+        request.getRequestDispatcher("WEB-INF/jsp/user/member_update_input.jsp").forward(request, response);
+    }
 }
