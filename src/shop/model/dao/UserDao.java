9ba215ee813402a9b3b6a14cb393940c6ba5cc0e
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-14 19:18:09 +0900

    fix:メソッドの位置変更、修正 #1

--- src/shop/model/dao/UserDao.java
+++ src/shop/model/dao/UserDao.java
@@ -9,6 +9,44 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao extends DaoBase {
+    public MemberBeans fetchMemberLogin(String memberMail, String password) {
+        PreparedStatement stmt        = null;
+        ResultSet         rs          = null;
+        MemberBeans       memberBeans = null;
+
+        try {
+            this.connect();
+            stmt = con.prepareStatement("SELECT * FROM member WHERE member_mail = ? AND member_password = ?");
+            stmt.setString(1, memberMail);
+            stmt.setString(2, password);
+            rs = stmt.executeQuery();
+            rs.next();
+
+            memberBeans = new MemberBeans();
+            memberBeans.setMemberMail(rs.getString("member_mail"));
+            memberBeans.setMemberPassword(rs.getString("member_password"));
+            memberBeans.setMemberName(rs.getString("member_name"));
+            memberBeans.setPostalCode(rs.getString("postal_code"));
+            memberBeans.setAddress(rs.getString("address"));
+            memberBeans.setTell(rs.getString("tell"));
+            memberBeans.setCreditCard(rs.getString("credit_card"));
+            memberBeans.setExpirationDate(rs.getString("expiration_date"));
+            memberBeans.setHolder(rs.getString("holder"));
+            memberBeans.setSecurityCode(rs.getString("security_code"));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                this.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return memberBeans;
+    }
+
     public boolean checkMemberMailExists(String memberMail) {
         PreparedStatement stmt = null;
         ResultSet         rs   = null;
@@ -117,49 +155,6 @@ public class UserDao extends DaoBase {
             }
         }
         return deleteLine != 0;
-    }
-
-    public MemberBeans fetchMemberLogin(String memberMail, String password) {
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
-        //戻り値を初期化
-        MemberBeans memberBeans = null;
-        try {
-            //DBのオープン
-            this.connect();
-            //SQLを作成
-            String sql = "SELECT * FROM member WHERE member_mail = ? AND member_password = ?";
-            stmt = con.prepareStatement(sql);
-            stmt.setString(1, memberMail);
-            stmt.setString(2, password);
-
-            //SQLの発行
-            rs = stmt.executeQuery();
-            rs.next();
-
-            //beanへ格納(検索失敗ならrs.nextでcatchへ飛ぶ)
-            memberBeans = new MemberBeans();
-            memberBeans.setMemberMail(rs.getString("member_mail"));
-            memberBeans.setMemberName(rs.getString("member_name"));
-            memberBeans.setMemberPassword(rs.getString("member_password"));
-            memberBeans.setPostalCode(rs.getInt("postal_code"));
-            memberBeans.setAddress(rs.getString("address"));
-            memberBeans.setTell(rs.getInt("tell"));
-            memberBeans.setCreditCard(rs.getInt("credit_card"));
-            memberBeans.setExpirationDate(rs.getString("expiration_date"));
-            memberBeans.setSecurityCode(rs.getInt("security_code"));
-            memberBeans.setHolder(rs.getString("holder"));
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                this.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return memberBeans;
     }
 
 
