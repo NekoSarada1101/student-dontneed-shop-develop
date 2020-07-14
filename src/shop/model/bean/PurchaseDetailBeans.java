1b3f397ca172ef49d709c50f0ae6bf312cfed0ac
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-14 12:01:01 +0900

    create:購入明細のBeans #42

---
+++ src/shop/model/bean/PurchaseDetailBeans.java
@@ -0,0 +1,43 @@
+package shop.model.bean;
+
+import java.io.Serializable;
+
+public class PurchaseDetailBeans implements Serializable {
+    private int purchaseId = 0;
+    private String memberMail = null;
+    private int productId = 0;
+    private String purchaseDate = null;
+
+
+    public int getPurchaseId() {
+        return purchaseId;
+    }
+
+    public void setPurchaseId(int purchaseId) {
+        this.purchaseId = purchaseId;
+    }
+
+    public String getMemberMail() {
+        return memberMail;
+    }
+
+    public void setMemberMail(String memberMail) {
+        this.memberMail = memberMail;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+}
