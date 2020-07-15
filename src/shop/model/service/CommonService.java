1e009229f11bed4008290b03a35d85ef890ad4a6
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-15 14:15:03 +0900

    update:入力チェック用メソッド作成 #97

--- src/shop/model/service/CommonService.java
+++ src/shop/model/service/CommonService.java
@@ -1,4 +1,17 @@
 package shop.model.service;
 
 public class CommonService {
+
+    public boolean checkLength(Object input, int maxLength, int minLength) {
+        String inputText = (String) input;
+        if (inputText.length() <= maxLength && inputText.length() >= minLength)
+            return true;
+        return false;
+    }
+
+    public boolean checkNull(Object input) {
+        if (input == null)
+            return false;
+        return true;
+    }
 }
