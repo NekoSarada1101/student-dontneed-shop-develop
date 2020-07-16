743cf05affc23c83108cbc19031be18f9a8ebf87
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-16 11:11:13 +0900

    create:escapeProcessメソッド作成 #98

--- src/shop/model/service/CommonService.java
+++ src/shop/model/service/CommonService.java
@@ -14,4 +14,14 @@ public class CommonService {
             return false;
         return true;
     }
+
+    public String escapeProcess(String str) {
+        str = str.replace("&", "&amp");
+        str = str.replace("<", "&lt");
+        str = str.replace(">", "&gt");
+        str = str.replace("\"", "&quot");
+        str = str.replace("\'", "&#39");
+        str = str.replace(" ", "&nbsp");
+        return str;
+    }
 }
