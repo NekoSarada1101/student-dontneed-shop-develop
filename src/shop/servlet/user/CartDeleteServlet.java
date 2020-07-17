652b600e360cb8ab8e22186997b66f291a24a98a
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-17 15:01:00 +0900

    fix:カート削除機能日修正

--- src/shop/servlet/user/CartDeleteServlet.java
+++ src/shop/servlet/user/CartDeleteServlet.java
@@ -1,8 +1,8 @@
 package shop.servlet.user;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import shop.model.bean.MemberBeans;
+import shop.model.bean.ProductBeans;
+import shop.model.service.PurchaseService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,10 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import shop.model.bean.MemberBeans;
-import shop.model.bean.ProductBeans;
-import shop.model.service.PurchaseService;
+import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/cartDelete")
 public class CartDeleteServlet extends HttpServlet {
@@ -21,19 +19,16 @@ public class CartDeleteServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	List<ProductBeans> cartList = new ArrayList<ProductBeans>();
-    	HttpSession session = request.getSession();
-    	cartList = (List<ProductBeans>)session.getAttribute("cartList");
-    	PurchaseService purchaseService = new PurchaseService();
+        HttpSession        session  = request.getSession();
+        List<ProductBeans> cartList = (List<ProductBeans>) session.getAttribute("productList");
 
+        String       memberMail   = ((MemberBeans) session.getAttribute("memberLoginInfo")).getMemberMail();
+        int          index        = Integer.parseInt(request.getParameter("index"));
+        ProductBeans productBeans = cartList.get(index);
+        int          productId    = productBeans.getProductId();
 
-    	String memberMail = ((MemberBeans) session.getAttribute("memberLoginInfo")).getMemberMail();
-    	int index = Integer.parseInt( request.getParameter("index"));
-    	ProductBeans productBeans = cartList.get(index);
-    	int productId = productBeans.getProductId();
-    	boolean couldDelete = purchaseService.deleteCart(memberMail, productId);
-    	request.setAttribute("couldDelete", couldDelete);
-		request.getRequestDispatcher("WEB-INF/jsp/user/cart_display.jsp").forward(request, response);
+        PurchaseService purchaseService = new PurchaseService();
+        boolean      couldDelete  = purchaseService.deleteCart(memberMail, productId);
+        response.sendRedirect("cartDisplay");
     }
-
 }
