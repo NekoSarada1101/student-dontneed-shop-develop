5dbf4ff98e770e207bb64bdcc22aa7ae89d27944
tomo0403 <1901301@s.asojuku.ac.jp>
2020-07-17 12:38:28 +0900

    #124 update 管理者・会員ログアウト機能

---
+++ src/shop/servlet/admin/AdminLogoutServlet.java
@@ -0,0 +1,27 @@
+package shop.servlet.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdminLogoutServlet
+ */
+@WebServlet("/AdminLogoutServlet")
+public class AdminLogoutServlet extends HttpServlet {
+		private static final long serialVersionUID = 1L;
+
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+			request.getSession().invalidate();
+			request.getRequestDispatcher("WEB-INF/jsp/admin/admin_login.jsp");
+
+		}
+
+
+}
+
