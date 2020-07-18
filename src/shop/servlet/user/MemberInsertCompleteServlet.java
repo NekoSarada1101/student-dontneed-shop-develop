f6d5219c4e5ff1ce5236cbab22949fd0d333b398
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-18 18:25:08 +0900

    update:MemberInsertCompleteServletログ出力 #130

--- src/shop/servlet/user/MemberInsertCompleteServlet.java
+++ src/shop/servlet/user/MemberInsertCompleteServlet.java
@@ -1,6 +1,9 @@
 package shop.servlet.user;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import shop.model.bean.MemberBeans;
+import shop.model.service.CommonService;
 import shop.model.service.UserService;
 
 import javax.servlet.ServletException;
@@ -16,23 +19,27 @@ import java.io.IOException;
 public class MemberInsertCompleteServlet extends HttpServlet {
 
     UserService userService = new UserService();
+    private Logger logger = LogManager.getLogger();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("{} Start", CommonService.getMethodName());
         HttpSession session = request.getSession();
 
         MemberBeans memberBeans = (MemberBeans) session.getAttribute("memberBeans");
+        logger.info("memberBeans={}", memberBeans);
+
 
         if (userService.checkMemberMailExists(memberBeans.getMemberMail())) {
             String errorMessage = "そのメールアドレスは既に登録済みです";
             request.setAttribute("errorMessage", errorMessage);
-
+            logger.trace("{} End", CommonService.getMethodName());
             request.getRequestDispatcher("WEB-INF/jsp/user/member_insert_input.jsp").forward(request, response);
             return;
         }
 
         userService.insertMember(memberBeans);
         session.removeAttribute("memberBeans");
-
+        logger.trace("{} End", CommonService.getMethodName());
         request.getRequestDispatcher("WEB-INF/jsp/user/member_insert_complete.jsp").forward(request, response);
     }
 }
