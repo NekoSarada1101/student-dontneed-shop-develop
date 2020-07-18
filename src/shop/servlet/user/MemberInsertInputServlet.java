a5a6aca1aafb8c6261f1913831892aeb56c0414b
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-18 18:25:20 +0900

    update:MemberInsertInputServletログ出力 #130

--- src/shop/servlet/user/MemberInsertInputServlet.java
+++ src/shop/servlet/user/MemberInsertInputServlet.java
@@ -1,5 +1,9 @@
 package shop.servlet.user;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import shop.model.service.CommonService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +14,11 @@ import java.io.IOException;
 @WebServlet("/memberInsertInput")
 public class MemberInsertInputServlet extends HttpServlet {
 
+    private Logger logger = LogManager.getLogger();
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("{} Start", CommonService.getMethodName());
+        logger.trace("{} End", CommonService.getMethodName());
         request.getRequestDispatcher("WEB-INF/jsp/user/member_insert_input.jsp").forward(request, response);
     }
 }
