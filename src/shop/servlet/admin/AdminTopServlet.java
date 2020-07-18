0d4bb4e3b4a3aee38fc269fa67fc3a24a1da3cb3
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-18 18:20:33 +0900

    update: AdminTopServletログ出力 #130

--- src/shop/servlet/admin/AdminTopServlet.java
+++ src/shop/servlet/admin/AdminTopServlet.java
@@ -1,7 +1,10 @@
 package shop.servlet.admin;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import shop.model.bean.AdminBeans;
 import shop.model.bean.ProductBeans;
+import shop.model.service.CommonService;
 import shop.model.service.ProductService;
 
 import javax.servlet.ServletException;
@@ -17,17 +20,21 @@ import java.util.List;
 public class AdminTopServlet extends HttpServlet {
 
     private ProductService productService = new ProductService();
+    private Logger         logger         = LogManager.getLogger();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("{} Start", CommonService.getMethodName());
         HttpSession session = request.getSession();
         String adminMail = ((AdminBeans) session.getAttribute("adminLoginInfo")).getAdminMail();
         List<ProductBeans> productList = productService.fetchAdminProductList(adminMail);
+        logger.info("adminMail={}", adminMail);
+        logger.info("productList={}", productList);
 
         session.removeAttribute("productBeans");
         session.removeAttribute("productList");
         session.setAttribute("productList", productList);
-
+        logger.trace("{} End", CommonService.getMethodName());
         request.getRequestDispatcher("WEB-INF/jsp/admin/admin_top.jsp").forward(request, response);
     }
 }
