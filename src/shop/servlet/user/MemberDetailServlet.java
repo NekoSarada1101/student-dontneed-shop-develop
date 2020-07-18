770b1065d71766e4d92ce70d6bd91655f7433129
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-18 18:24:31 +0900

    update:memberDetailServletログ出力 #130

--- src/shop/servlet/user/MemberDetailServlet.java
+++ src/shop/servlet/user/MemberDetailServlet.java
@@ -1,5 +1,9 @@
 package shop.servlet.user;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import shop.model.service.CommonService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,8 +14,12 @@ import java.io.IOException;
 @WebServlet("/memberDetail")
 public class MemberDetailServlet extends HttpServlet {
 
+    private Logger logger = LogManager.getLogger();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("{} Start", CommonService.getMethodName());
+        logger.trace("{} End", CommonService.getMethodName());
         request.getRequestDispatcher("WEB-INF/jsp/user/member_detail.jsp").forward(request, response);
     }
 }
