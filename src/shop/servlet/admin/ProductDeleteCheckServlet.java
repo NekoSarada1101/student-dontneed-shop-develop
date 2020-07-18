e389faa123f51c13c8161caf3773b7bf8a02ada5
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-18 18:21:24 +0900

    update: ProductDeleteCheckServletログ出力 #130

--- src/shop/servlet/admin/ProductDeleteCheckServlet.java
+++ src/shop/servlet/admin/ProductDeleteCheckServlet.java
@@ -1,6 +1,9 @@
 package shop.servlet.admin;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import shop.model.bean.ProductBeans;
+import shop.model.service.CommonService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,11 +17,16 @@ import java.util.List;
 @WebServlet("/productDeleteCheck")
 public class ProductDeleteCheckServlet extends HttpServlet {
 
+    private Logger logger = LogManager.getLogger();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("{} Start", CommonService.getMethodName());
         HttpSession session = request.getSession();
         List<ProductBeans> productList = (List<ProductBeans>) session.getAttribute("productList");
         int index = Integer.parseInt(request.getParameter("index"));
+        logger.info("productList={}", productList);
+        logger.info("index={}", index);
 
         ProductBeans productBeans = new ProductBeans();
         productBeans.setProductId(productList.get(index).getProductId());
@@ -30,7 +38,7 @@ public class ProductDeleteCheckServlet extends HttpServlet {
         productBeans.setGenreCode(productList.get(index).getGenreCode());
 
         session.setAttribute("productBeans", productBeans);
-
+        logger.trace("{} End", CommonService.getMethodName());
         request.getRequestDispatcher("WEB-INF/jsp/admin/product_delete_check.jsp").forward(request, response);
     }
 }
