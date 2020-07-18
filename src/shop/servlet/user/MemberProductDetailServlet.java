27aaca09186f4cb0585870432341fa14e927ab88
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-18 18:25:48 +0900

    update:MemberProductDetailServletログ出力 #130

--- src/shop/servlet/user/MemberProductDetailServlet.java
+++ src/shop/servlet/user/MemberProductDetailServlet.java
@@ -1,6 +1,9 @@
 package shop.servlet.user;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import shop.model.bean.ProductBeans;
+import shop.model.service.CommonService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,13 +17,17 @@ import java.util.List;
 @WebServlet("/memberProductDetail")
 public class MemberProductDetailServlet extends HttpServlet {
 
+    private Logger logger = LogManager.getLogger();
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("{} Start", CommonService.getMethodName());
         HttpSession session = request.getSession();
 
         int index = Integer.parseInt(request.getParameter("index"));
-
+        logger.info("index={}", index);
         List<ProductBeans> productList = (List<ProductBeans>) session.getAttribute("productList");
+        logger.info("productList={}", productList);
 
         ProductBeans productBeans = new ProductBeans();
         productBeans.setProductId(productList.get(index).getProductId());
@@ -30,9 +37,9 @@ public class MemberProductDetailServlet extends HttpServlet {
         productBeans.setProductExplanation(productList.get(index).getProductExplanation());
         productBeans.setIsSold(productList.get(index).getIsSold());
         productBeans.setGenreCode(productList.get(index).getGenreCode());
-
         session.setAttribute("productBeans", productBeans);
 
+        logger.trace("{} End", CommonService.getMethodName());
         request.getRequestDispatcher("WEB-INF/jsp/user/member_product_detail.jsp").forward(request, response);
     }
 }
