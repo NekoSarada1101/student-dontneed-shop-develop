9fae8b7efc7266ab02bc5e81b68d9944d7caed2c
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-18 18:24:09 +0900

    update:MemberDeleteCheckServletログ出力 #130

--- src/shop/servlet/user/MemberDeleteCheckServlet.java
+++ src/shop/servlet/user/MemberDeleteCheckServlet.java
@@ -1,18 +1,25 @@
 package shop.servlet.user;
 
-import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import shop.model.service.CommonService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/memberDeleteCheck")
 public class MemberDeleteCheckServlet extends HttpServlet {
 
+    private Logger logger = LogManager.getLogger();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("{} Start", CommonService.getMethodName());
+        logger.trace("{} End", CommonService.getMethodName());
         request.getRequestDispatcher("WEB-INF/jsp/admin/member_delete_check.jsp").forward(request, response);
     }
 }
