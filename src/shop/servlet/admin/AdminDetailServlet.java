f91092ffa44448a2a9c30cab0944b115669fcf69
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-19 20:45:20 +0900

    adminDerailServletリファクタ #142

--- src/shop/servlet/admin/AdminDetailServlet.java
+++ src/shop/servlet/admin/AdminDetailServlet.java
@@ -1,12 +1,15 @@
 package shop.servlet.admin;
 
-import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import shop.model.service.ErrorCheckService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Servlet implementation class AdminDetailsServlet
@@ -14,8 +17,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/adminDetail")
 public class AdminDetailServlet extends HttpServlet {
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		        request.getRequestDispatcher("WEB-INF/jsp/admin/admin_detail.jsp").forward(request, response);
-	}
+    private Logger logger = LogManager.getLogger();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("{} Start", ErrorCheckService.getMethodName());
+        logger.trace("{} End", ErrorCheckService.getMethodName());
+        request.getRequestDispatcher("WEB-INF/jsp/admin/admin_detail.jsp").forward(request, response);
+    }
 }
