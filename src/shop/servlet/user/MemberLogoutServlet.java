82ba624456fe4a4d1b49b1d17de5926a6f136e24
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-19 11:59:02 +0900

    update:invalidate()からremove()に変更 #124

--- src/shop/servlet/user/MemberLogoutServlet.java
+++ src/shop/servlet/user/MemberLogoutServlet.java
@@ -1,23 +1,23 @@
 package shop.servlet.user;
 
-import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
-@WebServlet("/MemberLogoutServlet")
+@WebServlet("/memberLogout")
 public class MemberLogoutServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.removeAttribute("memberLoginInfo");
+        session.removeAttribute("productList");
+        session.removeAttribute("productBeans");
+        session.removeAttribute("memberBeans");
 
-		request.getSession().invalidate();
-		request.getRequestDispatcher("WEB-INF/jsp/user/member_login.jsp");
-
-	}
-
-
+        request.getRequestDispatcher("WEB-INF/jsp/user/member_login.jsp");
+    }
 }
