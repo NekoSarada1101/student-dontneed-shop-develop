527db82f5a9dcdb2ec6057dcd90f4fc48930fce6
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-19 14:49:13 +0900

    update：AdminUpdateInputServlet修正 #128

--- src/shop/servlet/admin/AdminUpdateInputServlet.java
+++ src/shop/servlet/admin/AdminUpdateInputServlet.java
@@ -1,6 +1,9 @@
 package shop.servlet.admin;
 
-import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import shop.model.bean.AdminBeans;
+import shop.model.service.CommonService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,30 +11,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
-import shop.model.bean.AdminBeans;
-
-/**
- * Servlet implementation class AdminUpdateInputServlet
- */
-@WebServlet("/AdminUpdateInput")
+@WebServlet("/adminUpdateInput")
 public class AdminUpdateInputServlet extends HttpServlet {
 
+    private Logger logger = LogManager.getLogger();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session			= request.getSession();
-		AdminBeans adminBeans		= new AdminBeans();
-		AdminBeans adminLoginInfo	= (AdminBeans) session.getAttribute("adminLoginInfo");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("{} Start", CommonService.getMethodName());
+        HttpSession session        = request.getSession();
+        AdminBeans  adminBeans     = new AdminBeans();
+        AdminBeans  adminLoginInfo = (AdminBeans) session.getAttribute("adminLoginInfo");
 
-		adminBeans.setAdminMail(adminLoginInfo.getAdminMail());
-		adminBeans.setAdminPassword(adminLoginInfo.getAdminPassword());
-		adminBeans.setAdminName(adminLoginInfo.getAdminName());
-		adminBeans.setPostalCode(adminLoginInfo.getPostalCode());
-		adminBeans.setAddress(adminLoginInfo.getAddress());
+        logger.info("adminLoginInfo={}", adminLoginInfo);
 
-		session.setAttribute("adminBeans", adminBeans);
+        adminBeans.setAdminMail(adminLoginInfo.getAdminMail());
+        adminBeans.setAdminPassword(adminLoginInfo.getAdminPassword());
+        adminBeans.setAdminName(adminLoginInfo.getAdminName());
+        adminBeans.setPostalCode(adminLoginInfo.getPostalCode());
+        adminBeans.setAddress(adminLoginInfo.getAddress());
 
-		request.getRequestDispatcher("WEB-INF/jsp/admin/admin_update_input.jsp").forward(request, response);
-	}
-
+        session.setAttribute("adminBeans", adminBeans);
+        logger.trace("{} End", CommonService.getMethodName());
+        request.getRequestDispatcher("WEB-INF/jsp/admin/admin_update_input.jsp").forward(request, response);
+    }
 }
