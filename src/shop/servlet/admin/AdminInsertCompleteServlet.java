f5faf36f2da21a0e5d820edf7fc51135c1514c5c
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-19 20:45:44 +0900

    adminInsertCompleteServletリファクタ #142

--- src/shop/servlet/admin/AdminInsertCompleteServlet.java
+++ src/shop/servlet/admin/AdminInsertCompleteServlet.java
@@ -3,7 +3,7 @@ package shop.servlet.admin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import shop.model.bean.AdminBeans;
-import shop.model.service.CommonService;
+import shop.model.service.ErrorCheckService;
 import shop.model.service.UserService;
 
 import javax.servlet.ServletException;
@@ -21,23 +21,24 @@ public class AdminInsertCompleteServlet extends HttpServlet {
     private Logger logger = LogManager.getLogger();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        logger.trace("{} Start", CommonService.getMethodName());
-        HttpSession session = request.getSession();
+        logger.trace("{} Start", ErrorCheckService.getMethodName());
 
-        AdminBeans adminBeans = (AdminBeans) session.getAttribute("adminBeans");
+        HttpSession session    = request.getSession();
+        AdminBeans  adminBeans = (AdminBeans) session.getAttribute("adminBeans");
         logger.info("adminBeans={}", adminBeans);
 
         if (userService.checkAdminMailExists(adminBeans.getAdminMail())) {
             String errorMessage = "そのメールアドレスは既に登録済みです";
             request.setAttribute("errorMessage", errorMessage);
-            logger.trace("{} End", CommonService.getMethodName());
+            logger.trace("{} End", ErrorCheckService.getMethodName());
             request.getRequestDispatcher("WEB-INF/jsp/admin/admin_insert_input.jsp").forward(request, response);
             return;
         }
 
         userService.insertAdmin(adminBeans);
+
         session.removeAttribute("adminBeans");
-        logger.trace("{} End", CommonService.getMethodName());
+        logger.trace("{} End", ErrorCheckService.getMethodName());
         request.getRequestDispatcher("WEB-INF/jsp/admin/admin_insert_complete.jsp").forward(request, response);
     }
 }
