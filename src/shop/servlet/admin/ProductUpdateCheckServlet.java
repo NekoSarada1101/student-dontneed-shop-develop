1d15ea57b5f78992cc2b4c8f4d3bf3a503f8dd6f
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-19 20:47:54 +0900

    productUpdateServletリファクタ #142

--- src/shop/servlet/admin/ProductUpdateCheckServlet.java
+++ src/shop/servlet/admin/ProductUpdateCheckServlet.java
@@ -1,24 +1,18 @@
 
 package shop.servlet.admin;
 
-import java.io.IOException;
-import java.io.InputStream;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import shop.model.bean.ProductBeans;
+import shop.model.service.ErrorCheckService;
+import shop.model.service.ProductService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.Part;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import shop.model.bean.ProductBeans;
-import shop.model.service.CommonService;
-import shop.model.service.ProductService;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 @WebServlet("/productUpdateCheck")
 @MultipartConfig(location = "/tmp")
@@ -29,21 +23,23 @@ public class ProductUpdateCheckServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        logger.trace("{} Start", CommonService.getMethodName());
-        String productName = request.getParameter("productName");
-        Part filePart = request.getPart("image");
-        InputStream inputStream = filePart.getInputStream();
-        String productExplanation = request.getParameter("productExplanation");
-        int price;
-        int genreCode;
+        logger.trace("{} Start", ErrorCheckService.getMethodName());
+
+        String      productName        = request.getParameter("productName");
+        Part        filePart           = request.getPart("image");
+        InputStream inputStream        = filePart.getInputStream();
+        String      productExplanation = request.getParameter("productExplanation");
+        int         price;
+        int         genreCode;
         try {
             price = Integer.parseInt(request.getParameter("price"));
             genreCode = Integer.parseInt(request.getParameter("genre"));
         } catch (NumberFormatException e) {
             e.printStackTrace();
             logger.error("error={}", e);
-            logger.trace("{} End", CommonService.getMethodName());
-            request.getRequestDispatcher("WEB-INF/jsp/error.jsp").forward(request, response);
+            request.setAttribute("errorMessage", "不正な入力です");
+            logger.trace("{} End", ErrorCheckService.getMethodName());
+            request.getRequestDispatcher("WEB-INF/jsp/admin/product_update_input.jsp").forward(request, response);
             return;
         }
 
@@ -54,13 +50,13 @@ public class ProductUpdateCheckServlet extends HttpServlet {
         logger.info("genreCode={}", genreCode);
 
         if (!checkInputText(productName, price, productExplanation)) {
-            logger.trace("{} End", CommonService.getMethodName());
-            request.getRequestDispatcher("WEB-INF/jsp/error.jsp").forward(request, response);
+            request.setAttribute("errorMessage", "不正な入力です");
+            logger.trace("{} End", ErrorCheckService.getMethodName());
+            request.getRequestDispatcher("WEB-INF/jsp/admin/product_update_input.jsp").forward(request, response);
             return;
         }
 
-        HttpSession session = request.getSession();
-
+        HttpSession  session      = request.getSession();
         ProductBeans productBeans = (ProductBeans) session.getAttribute("productBeans");
         productBeans.setProductName(productName);
         productBeans.setPrice(price);
@@ -77,15 +73,14 @@ public class ProductUpdateCheckServlet extends HttpServlet {
         productBeans.setGenreCode(genreCode);
 
         session.setAttribute("productBeans", productBeans);
-        logger.trace("{} End", CommonService.getMethodName());
+        logger.trace("{} End", ErrorCheckService.getMethodName());
         request.getRequestDispatcher("WEB-INF/jsp/admin/product_update_check.jsp").forward(request, response);
     }
 
+
     public boolean checkInputText(String productName, int price, String productExplanation) {
-        if (!productService.checkLength(productName, 30, 1))
-            return false;
-        if (!productService.checkLength(productExplanation, 400, 1))
-            return false;
+        if (!ErrorCheckService.checkLength(productName, 30, 1)) return false;
+        if (!ErrorCheckService.checkLength(productExplanation, 400, 1)) return false;
         return true;
     }
 }
