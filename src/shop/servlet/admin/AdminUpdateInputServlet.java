c461da1e6d378e1eb35b3985c071db2456581c34
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-19 20:47:06 +0900

    adminUpdateServletリファクタ #142

--- src/shop/servlet/admin/AdminUpdateInputServlet.java
+++ src/shop/servlet/admin/AdminUpdateInputServlet.java
@@ -3,7 +3,7 @@ package shop.servlet.admin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import shop.model.bean.AdminBeans;
-import shop.model.service.CommonService;
+import shop.model.service.ErrorCheckService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,11 +19,11 @@ public class AdminUpdateInputServlet extends HttpServlet {
     private Logger logger = LogManager.getLogger();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        logger.trace("{} Start", CommonService.getMethodName());
+        logger.trace("{} Start", ErrorCheckService.getMethodName());
+
         HttpSession session        = request.getSession();
         AdminBeans  adminBeans     = new AdminBeans();
         AdminBeans  adminLoginInfo = (AdminBeans) session.getAttribute("adminLoginInfo");
-
         logger.info("adminLoginInfo={}", adminLoginInfo);
 
         adminBeans.setAdminMail(adminLoginInfo.getAdminMail());
@@ -33,7 +33,7 @@ public class AdminUpdateInputServlet extends HttpServlet {
         adminBeans.setAddress(adminLoginInfo.getAddress());
 
         session.setAttribute("adminBeans", adminBeans);
-        logger.trace("{} End", CommonService.getMethodName());
+        logger.trace("{} End", ErrorCheckService.getMethodName());
         request.getRequestDispatcher("WEB-INF/jsp/admin/admin_update_input.jsp").forward(request, response);
     }
 }
