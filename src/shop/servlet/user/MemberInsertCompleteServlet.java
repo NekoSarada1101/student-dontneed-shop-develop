83c0b40221b6253265e0fe2de3f598fcc3c31020
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-19 20:48:51 +0900

    memberInsertServletリファクタ #142

--- src/shop/servlet/user/MemberInsertCompleteServlet.java
+++ src/shop/servlet/user/MemberInsertCompleteServlet.java
@@ -3,7 +3,7 @@ package shop.servlet.user;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import shop.model.bean.MemberBeans;
-import shop.model.service.CommonService;
+import shop.model.service.ErrorCheckService;
 import shop.model.service.UserService;
 
 import javax.servlet.ServletException;
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-
 @WebServlet("/memberInsertComplete")
 public class MemberInsertCompleteServlet extends HttpServlet {
 
@@ -22,24 +21,23 @@ public class MemberInsertCompleteServlet extends HttpServlet {
     private Logger logger = LogManager.getLogger();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        logger.trace("{} Start", CommonService.getMethodName());
-        HttpSession session = request.getSession();
+        logger.trace("{} Start", ErrorCheckService.getMethodName());
 
+        HttpSession session = request.getSession();
         MemberBeans memberBeans = (MemberBeans) session.getAttribute("memberBeans");
         logger.info("memberBeans={}", memberBeans);
-
 
         if (userService.checkMemberMailExists(memberBeans.getMemberMail())) {
             String errorMessage = "そのメールアドレスは既に登録済みです";
             request.setAttribute("errorMessage", errorMessage);
-            logger.trace("{} End", CommonService.getMethodName());
+            logger.trace("{} End", ErrorCheckService.getMethodName());
             request.getRequestDispatcher("WEB-INF/jsp/user/member_insert_input.jsp").forward(request, response);
             return;
         }
 
         userService.insertMember(memberBeans);
         session.removeAttribute("memberBeans");
-        logger.trace("{} End", CommonService.getMethodName());
+        logger.trace("{} End", ErrorCheckService.getMethodName());
         request.getRequestDispatcher("WEB-INF/jsp/user/member_insert_complete.jsp").forward(request, response);
     }
 }
