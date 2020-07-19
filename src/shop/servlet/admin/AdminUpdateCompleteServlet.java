9fc1355cf4e10631ddcb0414c1bb34cb0800f12e
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-19 14:49:01 +0900

    update：AdminUpdateCompleteServlet修正 #128

--- src/shop/servlet/admin/AdminUpdateCompleteServlet.java
+++ src/shop/servlet/admin/AdminUpdateCompleteServlet.java
@@ -1,6 +1,10 @@
 package shop.servlet.admin;
 
-import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import shop.model.bean.AdminBeans;
+import shop.model.service.CommonService;
+import shop.model.service.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,21 +12,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
-import shop.model.bean.AdminBeans;
-
-
-@WebServlet("/AdminUpdateComplete")
+@WebServlet("/adminUpdateComplete")
 public class AdminUpdateCompleteServlet extends HttpServlet {
 
-	AdminService adminService = new AdminService();
+    UserService userService = new UserService();
+    private Logger logger = LogManager.getLogger();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session		= request.getSession();
-		AdminBeans adminBeans = (AdminBeans) session.getAttribute("adminBeans");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("{} Start", CommonService.getMethodName());
+        HttpSession session    = request.getSession();
+        AdminBeans  adminBeans = (AdminBeans) session.getAttribute("adminBeans");
 
-		if(!adminBeans.getAdminMail().equals(((AdminBeans) session.getAttribute("adminLoginInfo")).getAdminMail()))
-            if (adminService.checkAdminMailExists(adminBeans.getAdminMail())) {
+        logger.info("adminBeans={}", adminBeans);
+
+        if (!adminBeans.getAdminMail().equals(((AdminBeans) session.getAttribute("adminLoginInfo")).getAdminMail()))
+            if (userService.checkAdminMailExists(adminBeans.getAdminMail())) {
                 String errorMessage = "そのメールアドレスは既に登録済みです";
                 request.setAttribute("errorMessage", errorMessage);
 
@@ -30,9 +36,11 @@ public class AdminUpdateCompleteServlet extends HttpServlet {
                 return;
             }
 
-		adminService.updateAdmin(adminBeans);
-		session.setAttribute("adminLoginIngo", adminBeans);
-		session.removeAttribute("adminBeans");
-		request.getRequestDispatcher("WEB-INF/jsp/admin/admin_update_complete.jsp").forward(request, response);
+        userService.updateAdmin(adminBeans);
+        session.setAttribute("adminLoginInfo", adminBeans);
+        session.removeAttribute("adminBeans");
+        logger.trace("{} End", CommonService.getMethodName());
+        request.getRequestDispatcher("WEB-INF/jsp/admin/admin_update_complete.jsp").forward(request, response);
+    }
 }
-}
+
