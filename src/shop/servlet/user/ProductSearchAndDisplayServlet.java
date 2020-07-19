20bb9a70dd34c9f6f74ada7a16299bff08552be3
Sarada1101 <ryoyukikazu11@gmail.com>
2020-07-19 20:50:30 +0900

    productSearchAnddisplayServletリファクタ #142

--- src/shop/servlet/user/ProductSearchAndDisplayServlet.java
+++ src/shop/servlet/user/ProductSearchAndDisplayServlet.java
@@ -4,7 +4,7 @@ package shop.servlet.user;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import shop.model.bean.ProductBeans;
-import shop.model.service.CommonService;
+import shop.model.service.ErrorCheckService;
 import shop.model.service.ProductService;
 
 import javax.servlet.ServletException;
@@ -23,7 +23,7 @@ public class ProductSearchAndDisplayServlet extends HttpServlet {
     private Logger logger = LogManager.getLogger();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        logger.trace("{} Start", CommonService.getMethodName());
+        logger.trace("{} Start", ErrorCheckService.getMethodName());
         HttpSession session = request.getSession();
 
         int genreCode = Integer.parseInt(request.getParameter("genreCode"));
@@ -35,10 +35,18 @@ public class ProductSearchAndDisplayServlet extends HttpServlet {
         logger.info("sortOrder={}", sortOrder);
         logger.info("searchWord={}", searchWord);
 
+        if (!ErrorCheckService.checkAllowedSortColumn(sortColumn) || !ErrorCheckService.checkAllowedSortOrder(sortOrder)){
+            request.setAttribute("errorMessage", "不正な入力です");
+            logger.trace("{} End", ErrorCheckService.getMethodName());
+            request.getRequestDispatcher("WEB-INF/jsp/user/search_product_list.jsp").forward(request, response);
+            return;
+        }
+
         List<ProductBeans> productList = productService.fetchSearchProductList(genreCode, sortColumn, sortOrder, searchWord);
-        logger.info("productList={}", productList);
+        logger.info("productList.size={}", productList.size());
+
         session.setAttribute("productList", productList);
-        logger.trace("{} End", CommonService.getMethodName());
+        logger.trace("{} End", ErrorCheckService.getMethodName());
         request.getRequestDispatcher("WEB-INF/jsp/user/search_product_list.jsp").forward(request, response);
     }
 }
